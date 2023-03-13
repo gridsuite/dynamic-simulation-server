007b88d21fb64233dd61c75d0edd5f8cdec96cb7
Thang PHAM <phamthang37@gmail.com>
2023-03-13 16:43:58 +0100

    Remove unused import

--- src/test/java/org/gridsuite/ds/server/service/DynamicSimulationServiceTest.java
+++ src/test/java/org/gridsuite/ds/server/service/DynamicSimulationServiceTest.java
@@ -9,7 +9,6 @@ package org.gridsuite.ds.server.service;
 
 import com.fasterxml.jackson.core.JsonProcessingException;
 import com.fasterxml.jackson.databind.ObjectMapper;
-import org.gridsuite.ds.server.DynamicSimulationApplication;
 import org.gridsuite.ds.server.dto.DynamicSimulationStatus;
 import org.gridsuite.ds.server.model.ResultEntity;
 import org.gridsuite.ds.server.repository.ResultRepository;
