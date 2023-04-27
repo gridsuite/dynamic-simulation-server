2e351305e86a76932622d4b74a1c4c86fa46d363
Thang PHAM <phamthang37@gmail.com>
2023-04-27 16:48:15 +0200

    Java Docs and more timeout

--- src/test/java/org/gridsuite/ds/server/controller/AbstractDynawoTest.java
+++ src/test/java/org/gridsuite/ds/server/controller/AbstractDynawoTest.java
@@ -1,3 +1,10 @@
+/**
+ * Copyright (c) 2023, RTE (http://www.rte-france.com)
+ * This Source Code Form is subject to the terms of the Mozilla Public
+ * License, v. 2.0. If a copy of the MPL was not distributed with this
+ * file, You can obtain one at http://mozilla.org/MPL/2.0/.
+ */
+
 package org.gridsuite.ds.server.controller;
 
 import com.powsybl.computation.ComputationManager;
@@ -10,11 +17,15 @@ import java.io.IOException;
 import java.nio.file.Path;
 import java.nio.file.Paths;
 
+/**
+ * @author Thang PHAM <quyet-thang.pham at rte-france.com>
+ */
 public abstract class AbstractDynawoTest {
     private static final String DYNAWO_VERSION = "1.4.0";
 
     private static final String DOCKER_IMAGE_ID = "powsybl/java-dynawo:" + DYNAWO_VERSION;
 
+    // TODO wait junit5 to use @TempDir
     private final Path localDir = Paths.get("/tmp");
 
     protected ComputationManager computationManager;
