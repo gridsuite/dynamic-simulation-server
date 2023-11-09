e51ed631776fd593539ba232b242a218741d4ef0
Thang PHAM <phamthang37@gmail.com>
2023-11-09 13:07:44 +0100

    Avoid code duplication

--- src/main/java/org/gridsuite/ds/server/dto/solver/AbstractSolverInfos.java
+++ src/main/java/org/gridsuite/ds/server/dto/solver/AbstractSolverInfos.java
@@ -11,6 +11,10 @@ import lombok.Getter;
 import lombok.NoArgsConstructor;
 import lombok.Setter;
 import lombok.experimental.SuperBuilder;
+import org.gridsuite.ds.server.dto.XmlSerializableParameter;
+
+import javax.xml.stream.XMLStreamException;
+import javax.xml.stream.XMLStreamWriter;
 
 /**
  * @author Thang PHAM <quyet-thang.pham at rte-france.com>
@@ -20,6 +24,104 @@ import lombok.experimental.SuperBuilder;
 @Getter
 @Setter
 public abstract class AbstractSolverInfos implements SolverInfos {
+
+    public static final String FNORMTOL_ALG = "fnormtolAlg";
+    public static final String INITIALADDTOL_ALG = "initialaddtolAlg";
+    public static final String SCSTEPTOL_ALG = "scsteptolAlg";
+    public static final String MXNEWTSTEP_ALG = "mxnewtstepAlg";
+    public static final String MSBSET_ALG = "msbsetAlg";
+    public static final String MXITER_ALG = "mxiterAlg";
+    public static final String PRINTFL_ALG = "printflAlg";
+    public static final String FNORMTOL_ALG_J = "fnormtolAlgJ";
+    public static final String INITIALADDTOL_ALG_J = "initialaddtolAlgJ";
+    public static final String SCSTEPTOL_ALG_J = "scsteptolAlgJ";
+    public static final String MXNEWTSTEP_ALG_J = "mxnewtstepAlgJ";
+    public static final String MSBSET_ALG_J = "msbsetAlgJ";
+    public static final String MXITER_ALG_J = "mxiterAlgJ";
+    public static final String PRINTFL_ALG_J = "printflAlgJ";
+    public static final String FNORMTOL_ALG_INIT = "fnormtolAlgInit";
+    public static final String INITIALADDTOL_ALG_INIT = "initialaddtolAlgInit";
+    public static final String SCSTEPTOL_ALG_INIT = "scsteptolAlgInit";
+    public static final String MXNEWTSTEP_ALG_INIT = "mxnewtstepAlgInit";
+    public static final String MSBSET_ALG_INIT = "msbsetAlgInit";
+    public static final String MXITER_ALG_INIT = "mxiterAlgInit";
+    public static final String PRINTFL_ALG_INIT = "printflAlgInit";
+    public static final String MAXIMUM_NUMBER_SLOW_STEP_INCREASE = "maximumNumberSlowStepIncrease";
+    public static final String MINIMAL_ACCEPTABLE_STEP = "minimalAcceptableStep";
+
     private String id;
     private SolverTypeInfos type;
+
+    private double fnormtolAlg;
+
+    private double initialaddtolAlg;
+
+    private double scsteptolAlg;
+
+    private double mxnewtstepAlg;
+
+    private int msbsetAlg;
+
+    private int mxiterAlg;
+
+    private int printflAlg;
+
+    private double fnormtolAlgJ;
+
+    private double initialaddtolAlgJ;
+
+    private double scsteptolAlgJ;
+
+    private double mxnewtstepAlgJ;
+
+    private int msbsetAlgJ;
+
+    private int mxiterAlgJ;
+
+    private int printflAlgJ;
+
+    private double fnormtolAlgInit;
+
+    private double initialaddtolAlgInit;
+
+    private double scsteptolAlgInit;
+
+    private double mxnewtstepAlgInit;
+
+    private int msbsetAlgInit;
+
+    private int mxiterAlgInit;
+
+    private int printflAlgInit;
+
+    private int maximumNumberSlowStepIncrease;
+
+    private double minimalAcceptableStep;
+
+    @Override
+    public void writeParameter(XMLStreamWriter writer) throws XMLStreamException {
+        XmlSerializableParameter.writeParameter(writer, ParameterType.DOUBLE, FNORMTOL_ALG, Double.toString(fnormtolAlg));
+        XmlSerializableParameter.writeParameter(writer, ParameterType.DOUBLE, INITIALADDTOL_ALG, Double.toString(initialaddtolAlg));
+        XmlSerializableParameter.writeParameter(writer, ParameterType.DOUBLE, SCSTEPTOL_ALG, Double.toString(scsteptolAlg));
+        XmlSerializableParameter.writeParameter(writer, ParameterType.DOUBLE, MXNEWTSTEP_ALG, Double.toString(mxnewtstepAlg));
+        XmlSerializableParameter.writeParameter(writer, ParameterType.INT, MSBSET_ALG, Integer.toString(msbsetAlg));
+        XmlSerializableParameter.writeParameter(writer, ParameterType.INT, MXITER_ALG, Integer.toString(mxiterAlg));
+        XmlSerializableParameter.writeParameter(writer, ParameterType.INT, PRINTFL_ALG, Integer.toString(printflAlg));
+        XmlSerializableParameter.writeParameter(writer, ParameterType.DOUBLE, FNORMTOL_ALG_J, Double.toString(fnormtolAlgJ));
+        XmlSerializableParameter.writeParameter(writer, ParameterType.DOUBLE, INITIALADDTOL_ALG_J, Double.toString(initialaddtolAlgJ));
+        XmlSerializableParameter.writeParameter(writer, ParameterType.DOUBLE, SCSTEPTOL_ALG_J, Double.toString(scsteptolAlgJ));
+        XmlSerializableParameter.writeParameter(writer, ParameterType.DOUBLE, MXNEWTSTEP_ALG_J, Double.toString(mxnewtstepAlgJ));
+        XmlSerializableParameter.writeParameter(writer, ParameterType.INT, MSBSET_ALG_J, Integer.toString(msbsetAlgJ));
+        XmlSerializableParameter.writeParameter(writer, ParameterType.INT, MXITER_ALG_J, Integer.toString(mxiterAlgJ));
+        XmlSerializableParameter.writeParameter(writer, ParameterType.INT, PRINTFL_ALG_J, Integer.toString(printflAlgJ));
+        XmlSerializableParameter.writeParameter(writer, ParameterType.DOUBLE, FNORMTOL_ALG_INIT, Double.toString(fnormtolAlgInit));
+        XmlSerializableParameter.writeParameter(writer, ParameterType.DOUBLE, INITIALADDTOL_ALG_INIT, Double.toString(initialaddtolAlgInit));
+        XmlSerializableParameter.writeParameter(writer, ParameterType.DOUBLE, SCSTEPTOL_ALG_INIT, Double.toString(scsteptolAlgInit));
+        XmlSerializableParameter.writeParameter(writer, ParameterType.DOUBLE, MXNEWTSTEP_ALG_INIT, Double.toString(mxnewtstepAlgInit));
+        XmlSerializableParameter.writeParameter(writer, ParameterType.INT, MSBSET_ALG_INIT, Integer.toString(msbsetAlgInit));
+        XmlSerializableParameter.writeParameter(writer, ParameterType.INT, MXITER_ALG_INIT, Integer.toString(mxiterAlgInit));
+        XmlSerializableParameter.writeParameter(writer, ParameterType.INT, PRINTFL_ALG_INIT, Integer.toString(printflAlgInit));
+        XmlSerializableParameter.writeParameter(writer, ParameterType.INT, MAXIMUM_NUMBER_SLOW_STEP_INCREASE, Integer.toString(maximumNumberSlowStepIncrease));
+        XmlSerializableParameter.writeParameter(writer, ParameterType.DOUBLE, MINIMAL_ACCEPTABLE_STEP, Double.toString(minimalAcceptableStep));
+    }
 }
--- src/main/java/org/gridsuite/ds/server/dto/solver/SimSolverInfos.java
+++ src/main/java/org/gridsuite/ds/server/dto/solver/SimSolverInfos.java
@@ -45,30 +45,6 @@ public class SimSolverInfos extends AbstractSolverInfos {
     public static final String MINIMUM_MODE_CHANGE_TYPE_FOR_ALGEBRAIC_RESTORATION = "minimumModeChangeTypeForAlgebraicRestoration";
     public static final String MINIMUM_MODE_CHANGE_TYPE_FOR_ALGEBRAIC_RESTORATION_INIT = "minimumModeChangeTypeForAlgebraicRestorationInit";
 
-    public static final String FNORMTOL_ALG = "fnormtolAlg";
-    public static final String INITIALADDTOL_ALG = "initialaddtolAlg";
-    public static final String SCSTEPTOL_ALG = "scsteptolAlg";
-    public static final String MXNEWTSTEP_ALG = "mxnewtstepAlg";
-    public static final String MSBSET_ALG = "msbsetAlg";
-    public static final String MXITER_ALG = "mxiterAlg";
-    public static final String PRINTFL_ALG = "printflAlg";
-    public static final String FNORMTOL_ALG_J = "fnormtolAlgJ";
-    public static final String INITIALADDTOL_ALG_J = "initialaddtolAlgJ";
-    public static final String SCSTEPTOL_ALG_J = "scsteptolAlgJ";
-    public static final String MXNEWTSTEP_ALG_J = "mxnewtstepAlgJ";
-    public static final String MSBSET_ALG_J = "msbsetAlgJ";
-    public static final String MXITER_ALG_J = "mxiterAlgJ";
-    public static final String PRINTFL_ALG_J = "printflAlgJ";
-    public static final String FNORMTOL_ALG_INIT = "fnormtolAlgInit";
-    public static final String INITIALADDTOL_ALG_INIT = "initialaddtolAlgInit";
-    public static final String SCSTEPTOL_ALG_INIT = "scsteptolAlgInit";
-    public static final String MXNEWTSTEP_ALG_INIT = "mxnewtstepAlgInit";
-    public static final String MSBSET_ALG_INIT = "msbsetAlgInit";
-    public static final String MXITER_ALG_INIT = "mxiterAlgInit";
-    public static final String PRINTFL_ALG_INIT = "printflAlgInit";
-    public static final String MAXIMUM_NUMBER_SLOW_STEP_INCREASE = "maximumNumberSlowStepIncrease";
-    public static final String MINIMAL_ACCEPTABLE_STEP = "minimalAcceptableStep";
-
     // Important note: must using @JsonProperty to precise property's name when serialize/deserialize
     // fields which begin by a minuscule following by a majuscule, for example 'hMxxx', otherwise jackson
     // mapper will serialize as 'hmxxx' by default
@@ -111,52 +87,6 @@ public class SimSolverInfos extends AbstractSolverInfos {
 
     private String minimumModeChangeTypeForAlgebraicRestorationInit;
 
-    private double fnormtolAlg;
-
-    private double initialaddtolAlg;
-
-    private double scsteptolAlg;
-
-    private double mxnewtstepAlg;
-
-    private int msbsetAlg;
-
-    private int mxiterAlg;
-
-    private int printflAlg;
-
-    private double fnormtolAlgJ;
-
-    private double initialaddtolAlgJ;
-
-    private double scsteptolAlgJ;
-
-    private double mxnewtstepAlgJ;
-
-    private int msbsetAlgJ;
-
-    private int mxiterAlgJ;
-
-    private int printflAlgJ;
-
-    private double fnormtolAlgInit;
-
-    private double initialaddtolAlgInit;
-
-    private double scsteptolAlgInit;
-
-    private double mxnewtstepAlgInit;
-
-    private int msbsetAlgInit;
-
-    private int mxiterAlgInit;
-
-    private int printflAlgInit;
-
-    private int maximumNumberSlowStepIncrease;
-
-    private double minimalAcceptableStep;
-
     @Override
     public void writeParameter(XMLStreamWriter writer) throws XMLStreamException {
         writer.writeStartElement(DYN_BASE_URI, "set");
@@ -180,30 +110,8 @@ public class SimSolverInfos extends AbstractSolverInfos {
         XmlSerializableParameter.writeParameter(writer, ParameterType.BOOL, OPTIMIZE_REINIT_ALGEBRAIC_RESIDUALS_EVALUATIONS, Boolean.toString(optimizeReinitAlgebraicResidualsEvaluations));
         XmlSerializableParameter.writeParameter(writer, ParameterType.STRING, MINIMUM_MODE_CHANGE_TYPE_FOR_ALGEBRAIC_RESTORATION, minimumModeChangeTypeForAlgebraicRestoration);
         XmlSerializableParameter.writeParameter(writer, ParameterType.STRING, MINIMUM_MODE_CHANGE_TYPE_FOR_ALGEBRAIC_RESTORATION_INIT, minimumModeChangeTypeForAlgebraicRestorationInit);
-        XmlSerializableParameter.writeParameter(writer, ParameterType.DOUBLE, FNORMTOL_ALG, Double.toString(fnormtolAlg));
-        XmlSerializableParameter.writeParameter(writer, ParameterType.DOUBLE, INITIALADDTOL_ALG, Double.toString(initialaddtolAlg));
-        XmlSerializableParameter.writeParameter(writer, ParameterType.DOUBLE, SCSTEPTOL_ALG, Double.toString(scsteptolAlg));
-        XmlSerializableParameter.writeParameter(writer, ParameterType.DOUBLE, MXNEWTSTEP_ALG, Double.toString(mxnewtstepAlg));
-        XmlSerializableParameter.writeParameter(writer, ParameterType.INT, MSBSET_ALG, Integer.toString(msbsetAlg));
-        XmlSerializableParameter.writeParameter(writer, ParameterType.INT, MXITER_ALG, Integer.toString(mxiterAlg));
-        XmlSerializableParameter.writeParameter(writer, ParameterType.INT, PRINTFL_ALG, Integer.toString(printflAlg));
-        XmlSerializableParameter.writeParameter(writer, ParameterType.DOUBLE, FNORMTOL_ALG_J, Double.toString(fnormtolAlgJ));
-        XmlSerializableParameter.writeParameter(writer, ParameterType.DOUBLE, INITIALADDTOL_ALG_J, Double.toString(initialaddtolAlgJ));
-        XmlSerializableParameter.writeParameter(writer, ParameterType.DOUBLE, SCSTEPTOL_ALG_J, Double.toString(scsteptolAlgJ));
-        XmlSerializableParameter.writeParameter(writer, ParameterType.DOUBLE, MXNEWTSTEP_ALG_J, Double.toString(mxnewtstepAlgJ));
-        XmlSerializableParameter.writeParameter(writer, ParameterType.INT, MSBSET_ALG_J, Integer.toString(msbsetAlgJ));
-        XmlSerializableParameter.writeParameter(writer, ParameterType.INT, MXITER_ALG_J, Integer.toString(mxiterAlgJ));
-        XmlSerializableParameter.writeParameter(writer, ParameterType.INT, PRINTFL_ALG_J, Integer.toString(printflAlgJ));
-        XmlSerializableParameter.writeParameter(writer, ParameterType.DOUBLE, FNORMTOL_ALG_INIT, Double.toString(fnormtolAlgInit));
-        XmlSerializableParameter.writeParameter(writer, ParameterType.DOUBLE, INITIALADDTOL_ALG_INIT, Double.toString(initialaddtolAlgInit));
-        XmlSerializableParameter.writeParameter(writer, ParameterType.DOUBLE, SCSTEPTOL_ALG_INIT, Double.toString(scsteptolAlgInit));
-        XmlSerializableParameter.writeParameter(writer, ParameterType.DOUBLE, MXNEWTSTEP_ALG_INIT, Double.toString(mxnewtstepAlgInit));
-        XmlSerializableParameter.writeParameter(writer, ParameterType.INT, MSBSET_ALG_INIT, Integer.toString(msbsetAlgInit));
-        XmlSerializableParameter.writeParameter(writer, ParameterType.INT, MXITER_ALG_INIT, Integer.toString(mxiterAlgInit));
-        XmlSerializableParameter.writeParameter(writer, ParameterType.INT, PRINTFL_ALG_INIT, Integer.toString(printflAlgInit));
-        XmlSerializableParameter.writeParameter(writer, ParameterType.INT, MAXIMUM_NUMBER_SLOW_STEP_INCREASE, Integer.toString(maximumNumberSlowStepIncrease));
-        XmlSerializableParameter.writeParameter(writer, ParameterType.DOUBLE, MINIMAL_ACCEPTABLE_STEP, Double.toString(minimalAcceptableStep));
 
+        super.writeParameter(writer);
         writer.writeEndElement();
     }
 }
