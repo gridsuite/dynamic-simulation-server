dfad2b6ba92155fea41bbd6463cd729f1ba13f78
Thang PHAM <117309322+thangqp@users.noreply.github.com>
2023-11-13 12:15:44 +0100

    Dynamic Simulation : migrate with Powsybl Dynawo version 2.1.0 (#73)
    Co-authored-by: Gautier Bureau <gautier.bureau@rte-france.com>

--- src/main/java/org/gridsuite/ds/server/service/parameters/impl/ParametersServiceImpl.java
+++ src/main/java/org/gridsuite/ds/server/service/parameters/impl/ParametersServiceImpl.java
@@ -127,6 +127,12 @@ public class ParametersServiceImpl implements ParametersService {
                 }
 
                 dynaWaltzParameters.setNetworkParameters(networkParameters);
+
+                // Quick fix to make working in powsybl-dynawo 2.1.0
+                // Cannot invoke "com.powsybl.dynawaltz.DumpFileParameters.useDumpFile()"
+                // because "dumpFileParameters" is null
+                // TODO This param must be configured by default during the creation at the powsybl level
+                dynaWaltzParameters.setDefaultDumpFileParameters();
             }
 
             return parameters;
