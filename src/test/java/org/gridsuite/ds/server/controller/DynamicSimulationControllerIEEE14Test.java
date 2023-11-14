f99315327a866fb2c70c47bfd3fad597397a6d0e
Thang PHAM <phamthang37@gmail.com>
2023-11-14 15:12:44 +0100

    correct timeout

--- src/test/java/org/gridsuite/ds/server/controller/DynamicSimulationControllerIEEE14Test.java
+++ src/test/java/org/gridsuite/ds/server/controller/DynamicSimulationControllerIEEE14Test.java
@@ -212,7 +212,7 @@ public class DynamicSimulationControllerIEEE14Test extends AbstractDynamicSimula
         UUID runUuid = UUID.fromString(entityExchangeResult.getResponseBody().toString());
 
         //TODO maybe find a more reliable way to test this : failed with 1000 * 60 timeout
-        Message<byte[]> messageSwitch = output.receive(1500 * 6, dsResultDestination);
+        Message<byte[]> messageSwitch = output.receive(1000 * 60, dsResultDestination);
         assertEquals(runUuid, UUID.fromString(messageSwitch.getHeaders().get(DynamicSimulationResultContext.HEADER_RESULT_UUID).toString()));
 
         // --- CHECK result at abstract level --- //
