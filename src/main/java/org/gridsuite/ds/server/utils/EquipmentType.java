55e9a54c6c1f2dda93a3fa4554b48465d9169730
Thang PHAM <phamthang37@gmail.com>
2024-01-11 18:43:09 +0100

    Add BUSBAR_SECTION equipment type as static type

--- src/main/java/org/gridsuite/ds/server/utils/EquipmentType.java
+++ src/main/java/org/gridsuite/ds/server/utils/EquipmentType.java
@@ -4,9 +4,10 @@ public enum EquipmentType {
     GENERATOR,
     LOAD,
     BUS,
+    BUSBAR_SECTION,
     STATIC_VAR_COMPENSATOR;
 
     public static boolean isStaticType(EquipmentType equipmentType) {
-        return equipmentType == BUS;
+        return equipmentType == BUS || equipmentType == BUSBAR_SECTION;
     }
 }
