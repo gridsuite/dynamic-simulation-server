ef5b9418996f9efeec92bb0b8031b536056f48dc
Hugo Marcellin <hugo.marcelin@rte-france.com>
2025-12-04 15:08:42 +0100

    Increase coverage

--- src/test/java/org/gridsuite/ds/server/error/DynamicSimulationExceptionHandlerTest.java
+++ src/test/java/org/gridsuite/ds/server/error/DynamicSimulationExceptionHandlerTest.java
@@ -7,6 +7,7 @@
 package org.gridsuite.ds.server.error;
 
 import com.powsybl.ws.commons.error.PowsyblWsProblemDetail;
+import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 import org.springframework.http.HttpStatus;
@@ -14,13 +15,14 @@ import org.springframework.http.ResponseEntity;
 import org.springframework.mock.web.MockHttpServletRequest;
 
 import static org.assertj.core.api.AssertionsForClassTypes.assertThat;
+import static org.gridsuite.ds.server.error.DynamicSimulationBusinessErrorCode.MAPPING_NOT_LAST_RULE_WITH_EMPTY_FILTER_ERROR;
 import static org.gridsuite.ds.server.error.DynamicSimulationBusinessErrorCode.MAPPING_NOT_PROVIDED;
-import static org.junit.Assert.assertEquals;
+import static org.gridsuite.ds.server.error.DynamicSimulationBusinessErrorCode.PROVIDER_NOT_FOUND;
 
 /**
  * @author Hugo Marcellin <hugo.marcelin at rte-france.com>
  */
-public class DynamicSimulationExceptionHandlerTest {
+class DynamicSimulationExceptionHandlerTest {
     private DynamicSimulationExceptionHandler handler;
 
     @BeforeEach
@@ -29,13 +31,35 @@ public class DynamicSimulationExceptionHandlerTest {
     }
 
     @Test
-    void mapsBadRequestErrorBusinessErrorToStatus() {
-        MockHttpServletRequest request = new MockHttpServletRequest("GET", "/results-endpoint/uuid");
+    void mapsMappingNotProvidedErrorBusinessErrorToStatus() {
+        MockHttpServletRequest request = new MockHttpServletRequest("GET", "/api/v1/dynamic-simulation/mappings");
         DynamicSimulationException exception = new DynamicSimulationException(MAPPING_NOT_PROVIDED, "Mapping not provided");
         ResponseEntity<PowsyblWsProblemDetail> response = handler.handleDynamicSimulationException(exception, request);
 
         assertThat(response.getStatusCode()).isEqualTo(HttpStatus.BAD_REQUEST);
         assertThat(response.getBody()).isNotNull();
-        assertEquals("dynamicSimulation.mappingNotProvided", response.getBody().getBusinessErrorCode());
+        Assertions.assertEquals("dynamicSimulation.mappingNotProvided", response.getBody().getBusinessErrorCode());
+    }
+
+    @Test
+    void mapsMappingNotLastRuleWithEmptyFilterErrorBusinessErrorToStatus() {
+        MockHttpServletRequest request = new MockHttpServletRequest("GET", "/api/v1/dynamic-simulation/mappings");
+        DynamicSimulationException exception = new DynamicSimulationException(MAPPING_NOT_LAST_RULE_WITH_EMPTY_FILTER_ERROR, "Only last rule can have empty filter: type SUBSTATION rule index 0.");
+        ResponseEntity<PowsyblWsProblemDetail> response = handler.handleDynamicSimulationException(exception, request);
+
+        assertThat(response.getStatusCode()).isEqualTo(HttpStatus.BAD_REQUEST);
+        assertThat(response.getBody()).isNotNull();
+        Assertions.assertEquals("dynamicSimulation.mappingNotLastRuleWithEmptyFilterError", response.getBody().getBusinessErrorCode());
+    }
+
+    @Test
+    void mapsBadRequestErrorBusinessErrorToStatus() {
+        MockHttpServletRequest request = new MockHttpServletRequest("GET", "/api/v1/dynamic-simulation");
+        DynamicSimulationException exception = new DynamicSimulationException(PROVIDER_NOT_FOUND, "Dynamic simulation provider not found");
+        ResponseEntity<PowsyblWsProblemDetail> response = handler.handleDynamicSimulationException(exception, request);
+
+        assertThat(response.getStatusCode()).isEqualTo(HttpStatus.NOT_FOUND);
+        assertThat(response.getBody()).isNotNull();
+        Assertions.assertEquals("dynamicSimulation.providerNotFound", response.getBody().getBusinessErrorCode());
     }
 }
